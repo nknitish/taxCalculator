c9ba168cacb7affbc3ef51436bd1a0e0d09267ce
Nitish Kumar <nitkumar30@WKMZT49F89C6.global.publicisgroupe.net>
2022-12-17 14:49:44 +0530

    working on reset button

--- src/components/atom/InputField/InputField.jsx
+++ src/components/atom/InputField/InputField.jsx
@@ -1,11 +1,13 @@
 import { OutlinedInput, styled } from "@mui/material";
 
-const InputField = styled((props) => (
-  <OutlinedInput fullWidth type="number" {...props} />
-))`
+const StyleInputField = styled(OutlinedInput)`
   margin-top: 20px;
   height: 30px;
   border-radius: 20px;
 `;
+
+const InputField = (props) => {
+  return <StyleInputField fullWidth type="number" {...props} />;
+};
 
 export { InputField };
